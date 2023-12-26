31afcfb7850f82164aa23783f0f7429491bec5df
ElMihael666 <ahmadeev.mihael666@yandex.ru>
2023-12-26 17:01:36 +0500

    Pulse DONE Fix #1 26.12

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1500,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" alt="left"/></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg" alt="right"/></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="left"/></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="right"/></button>',
         // autoplay: true,
         // autoplaySpeed: 4000,
         dotsClass: 'slick-dots',
